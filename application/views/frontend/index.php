bd533d2007aca6db3cc0d6d090e0c0f758692624
Dhimas Panji Sastra <dhimaspanji66@gmail.com>
2023-03-21 09:35:30 +0700

    perbaikan tampilan

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -115,7 +115,7 @@
 				</div>
 
 				<!-- Slide 2 -->
-				<div class="carousel-item" style="background-image: url(assets/front/img/slide/HIPERBARIK.jpg);">
+				<div class="carousel-item" style="background-image: url(assets/front/img/slide/ea.jpg)">
 
 
 
@@ -129,17 +129,15 @@
 				</div>
 
 				<!-- Slide 3 -->
-				<div class="carousel-item" style="background-image: url(assets/front/img/slide/04.jpg)">
-					<!-- <div class="container">
-            <h2>Sequi ea ut et est quaerat</h2>
-            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam.
-              Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus
-              deleniti vel.</p>
-            <a href="#about" class="btn-get-started scrollto">Read More</a>
-          </div> -->
+				<div class="carousel-item" style="background-image: url(assets/front/img/slide/layanan_unggulan1.png)">
+					<div class="container">
+						<h2>Layanan Unggulan</h2>
+						<p>Layanan unggulan kami yang sangat unggulan</p>
+						<a href="#featured-services" class="btn-get-started scrollto">Read More</a>
+					</div>
 				</div>
 				<!-- Slide 4 -->
-				<div class="carousel-item" style="background-image: url(assets/front/img/slide/ea.jpg)">
+				<div class="carousel-item" style="background-image: url(assets/front/img/slide/poli_jantung2.png)">
 					<!-- <div class="container">
             <h2>Sequi ea ut et est quaerat</h2>
             <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam.
@@ -497,7 +495,7 @@
 						?>
 							<div class="col-sm-6 col-md-4">
 								<div class="card">
-									<img src="<?php echo base_url('gambar/artikel/') . $value->sampul ?>" class="card-img-top" alt="...">
+									<img src="<?php echo base_url('gambar/artikel/') . $value->sampul ?>" class="card-img-top" style="height: 250px;" alt="...">
 									<div class="card-body">
 										<h5 class="card-title"><?php echo  $value->judul ?></h5>
 
