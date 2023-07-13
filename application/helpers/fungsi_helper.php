d9feffa39abea9aa8670c276e52f14700db2855a
basyit.ashqeen28@gmail.com <basyit.ashqeen28@gmail.com>
2023-07-13 08:20:07 +0700

    fungsi

--- application/helpers/fungsi_helper.php
+++ application/helpers/fungsi_helper.php
@@ -11,7 +11,7 @@ function check_not_login()
 {
     $ci = &get_instance();
     $user_session = $ci->session->userdata('id');
-    if (!$user_session) {
+    if (!$user_session) { 
         redirect('Auth');
     }
 }
