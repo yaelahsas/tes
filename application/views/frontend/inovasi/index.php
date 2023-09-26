6b4d5ace9bae842d5a1ba8d74eefd0c5805d5174
Dhimas Panji Sastra <dhimaspanji66@gmail.com>
2023-09-26 13:10:51 +0700

    perbaikan

--- application/views/frontend/inovasi/index.php
+++ application/views/frontend/inovasi/index.php
@@ -37,9 +37,11 @@
 			<div class="col-md-4">
 				<div class="card">
 					<div class="card-body">
-						<h5 class="card-title">PIOVAR</h5>
+						<h5 class="card-title">PIOFAR</h5>
 						<p class="card-text">Pelayanan informasi obat instalasi farmasi</p>
-						<button class="btn btn-primary">Klik</button>
+						<button class="btn btn-primary">
+							<a href="https://wa.me/6281235117253?text=Saya%20ingin%20bertanya%20tentang%20obat" style="color: white;">Hubungi Kami</a>
+						</button>
 					</div>
 				</div>
 			</div>
@@ -48,7 +50,7 @@
 					<div class="card-body">
 						<h5 class="card-title">GERAKPOL KANGMAS</h5>
 						<p class="card-text">Konsultasi layanan poliklinik</p>
-						<button class="btn btn-primary">Klik</button>
+						<button class="btn btn-primary"><a href="https://wa.me/6282228748899?text=Halo,%20saya%20ingin%20bertanya%20mengenai%20poliklinik" style="color: white;">Hubungi Kami</a></button>
 					</div>
 				</div>
 			</div>
@@ -59,7 +61,8 @@
 					<div class="card-body">
 						<h5 class="card-title">SEMBUH CERIA</h5>
 						<p class="card-text">Konsultasi perawatan rawat inap kelas 1</p>
-						<button class="btn btn-primary">Klik</button>
+						<button class="btn btn-primary"><a href="https://wa.me/6281131194118?text=Halo,%20saya%20pasien%20pasca%20rawat%20inap%20Ruang%20Kelas%201" style="color: white;">Hubungi Kami</a>
+						</button>
 					</div>
 				</div>
 			</div>
@@ -70,6 +73,17 @@
 						<p class="card-text">Konsultasi pola kepatuhan cairan pasien HD</p>
 						<button class="btn btn-primary">Klik</button>
 					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row justify-content-center"> <!-- Menggunakan justify-content-center untuk tengah horizontal -->
+		<div class="col-md-1">
+			<div class="card custom-card">
+				<div class="card-body">
+					<a href='http://www.freevisitorcounters.com'>at Freevisitorcounters.com</a>
+					<script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=bdf3fe564d5a0b284c2b63bd9a19a06c396007ba'></script>
+					<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1079940/t/7"></script>
 				</div>
 			</div>
 		</div>
