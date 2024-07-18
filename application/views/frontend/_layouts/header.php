36e649e4460b9858eea5054a54846688eb338aa2
Dhimas Panji Sastra <dhimaspanji66@gmail.com>
2024-07-18 08:48:36 +0700

    komores gambar

--- application/views/frontend/_layouts/header.php
+++ application/views/frontend/_layouts/header.php
@@ -5,14 +5,12 @@
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-	<title>RSUD Genteng</title>
-	<meta content="" name="description">
-	<meta content="" name="keywords">
+	<title>RSUD Genteng - Rumah Sakit Umum Daerah Terbaik</title>
+	<meta content="RSUD Genteng adalah rumah sakit umum daerah yang menyediakan pelayanan kesehatan terbaik dengan berbagai fasilitas medis dan dokter spesialis." name="description">
+	<meta content="RSUD Genteng, rumah sakit, pelayanan kesehatan, dokter spesialis, fasilitas medis, rumah sakit umum daerah" name="keywords">
 
 	<!-- Favicons -->
 	<link href="<?php echo base_url('assets/img/logo_bwi_small.png'); ?>" rel="shortcut icon">
-
-
 
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -28,33 +26,20 @@
 	<link href="<?php echo base_url('assets/front/vendor/swiper/swiper-bundle.min.css'); ?>" rel="stylesheet">
 
 	<!-- Template Main CSS File -->
-
 	<link href="<?php echo base_url('assets/front/css/style.css'); ?>" rel="stylesheet">
 
-	<!-- =======================================================
-  * Template Name: Medicio - v4.10.0
-  * Template URL: https://bootstrapmade.com/medicio-free-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 	<style>
 		/* Tambahkan di dalam tag <style> atau dalam file CSS terpisah */
 		.fixed-height {
 			height: 300px;
-			/* Ubah angka sesuai dengan ketinggian yang Anda inginkan */
 			object-fit: cover;
-			/* Gambar akan diatur untuk mengisi ruang dengan menjaga proporsi */
 			width: 100%;
-			/* Pastikan gambar mengisi lebar elemen parent */
 		}
 
 		.fixed-artikel {
 			height: 400px;
-			/* Ubah angka sesuai dengan ketinggian yang Anda inginkan */
 			object-fit: cover;
-			/* Gambar akan diatur untuk mengisi ruang dengan menjaga proporsi */
 			width: 100%;
-			/* Pastikan gambar mengisi lebar elemen parent */
 		}
 
 		.member {
@@ -67,18 +52,15 @@
 
 		.member-img {
 			width: 100%;
-			/* Pastikan gambar mengisi lebar elemen parent */
 		}
 
 		.member-info {
 			padding: 10px;
-			/* Atur jarak antara nama dokter dan gambar */
 		}
 
 		.iframe-wrapper {
 			position: relative;
 			padding-bottom: 56.25%;
-			/* Aspek rasio 16:9 */
 			height: 900px;
 			overflow: hidden;
 		}
@@ -89,44 +71,29 @@
 			left: 0;
 			width: 100%;
 			height: 100%;
-			/* Tetapkan tinggi iframe 100% */
 		}
 
-
-
 		@media (min-width: 1023px) {
-
-			/* Gaya untuk layar antara 768px dan 1023px */
 			#hero {
 				height: 720px !important;
-				/* Set the height to 50% of the viewport height */
 			}
 		}
 
 		@media (min-width: 768px) and (max-width: 1023px) {
-
-			/* Gaya untuk layar antara 768px dan 1023px */
 			#hero {
 				height: 67vh;
-				/* Set the height to 50% of the viewport height */
 			}
 		}
 
-
-		/* Adjust the carousel height for mobile devices */
-		/* For screens smaller than 768px (tablets, phones) */
 		@media (max-width: 768px) {
 			#hero {
 				height: 57vh;
-				/* Set the height to 50% of the viewport height */
 			}
 		}
 
-		/* For screens smaller than 576px (small phones) */
 		@media (max-width: 576px) {
 			#hero {
 				height: 40vh;
-				/* Set the height to 40% of the viewport height */
 			}
 		}
 	</style>
@@ -149,14 +116,10 @@
 	<!-- ======= Header ======= -->
 	<header id="header" class="fixed-top">
 		<div class="container d-flex align-items-center">
-
-			<a href<?= base_url() ?>"" class="logo me-auto"><img src="<?php echo base_url('assets/front/img/logo-dark.png') ?>" alt=""></a>
-			<!-- Uncomment below if you prefer to use an image logo -->
-			<!-- <h1 class="logo me-auto"><a href="index.html">Medicio</a></h1> -->
-
+			<a href="<?= base_url() ?>" class="logo me-auto"><img src="<?php echo base_url('assets/front/img/logo-dark.png') ?>" alt="Logo RSUD Genteng" title="RSUD Genteng"></a>
 			<nav id="navbar" class="navbar order-last order-lg-0">
 				<ul>
-					<li><a class="nav-link scrollto " href="<?= base_url() ?>">Home</a></li>
+					<li><a class="nav-link scrollto" href="<?= base_url() ?>">Home</a></li>
 					<li class="dropdown"><a href="#"><span>Tentang</span> <i class="bi bi-chevron-down"></i></a>
 						<ul>
 							<li><a href="<?= base_url('tentang/struktur_organisasi') ?>">Struktur Organisasi</a></li>
@@ -165,11 +128,9 @@
 					<li><a class="nav-link scrollto" href="<?= base_url() ?>#services">Pelayanan</a></li>
 					<li><a class="nav-link scrollto" href="<?= base_url() ?>#doctors">Dokter</a></li>
 					<li><a class="nav-link scrollto" href="<?= base_url() ?>#contact">Kontak</a></li>
-					<li><a class="nav-link " href="<?= base_url('home/inovasi') ?>">Inovasi</a></li>
+					<li><a class="nav-link" href="<?= base_url('home/inovasi') ?>">Inovasi</a></li>
 				</ul>
 				<i class="bi bi-list mobile-nav-toggle"></i>
 			</nav><!-- .navbar -->
-
-
 		</div>
-	</header><!-- End Header -->
+	</header>
